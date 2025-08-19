99c8ef33ec665a89e9d844a574d207992f7674a3
Ayush Kashyap <kashyap11ayush02@gmail.com>
2025-08-19 14:43:14 +0530

    Update redis.mjs

--- redis.mjs
+++ redis.mjs
@@ -10,7 +10,7 @@ const client = createClient({
     port: parseInt(process.env.REDIS_PORT || '16649')
   }
 });
-
+// adding random comment 
 client.on('error', err => console.error('❌ Redis Client Error', err));
 client.on('connect', () => console.log('✅ Connected to Redis'));
 client.on('ready', () => console.log('🚀 Redis Client Ready'));
